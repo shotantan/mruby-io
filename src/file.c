0b0bce6d3495d6f00ca086bf1624d8b282df0a2d
Shota Nakano <shota.nakano@manycolors-inc.com>
2016-03-09 16:53:07 +0900

    add atime, ctime, mtime

--- src/file.c
+++ src/file.c
@@ -303,7 +303,7 @@ mrb_file_atime(mrb_state *mrb, mrb_value k)
   result = mrb_str_buf_new(mrb, PATH_MAX);
   if (stat(cpath, &stat_buf) != 0)
     mrb_sys_fail(mrb, cpath);
-  strcpy(RSTRING_PTR(result), ctime(&stat_buf.st_atime))
+  strcpy(RSTRING_PTR(result), ctime(&stat_buf.st_atime));
   mrb_str_resize(mrb, result, strlen(RSTRING_PTR(result)));
   return result;
 }
@@ -328,7 +328,7 @@ mrb_file_ctime(mrb_state *mrb, mrb_value k)
   result = mrb_str_buf_new(mrb, PATH_MAX);
   if (stat(cpath, &stat_buf) != 0)
     mrb_sys_fail(mrb, cpath);
-  strcpy(RSTRING_PTR(result), ctime(&stat_buf.st_ctime))
+  strcpy(RSTRING_PTR(result), ctime(&stat_buf.st_ctime));
   mrb_str_resize(mrb, result, strlen(RSTRING_PTR(result)));
   return result;
 }
@@ -353,7 +353,7 @@ mrb_file_mtime(mrb_state *mrb, mrb_value k)
   result = mrb_str_buf_new(mrb, PATH_MAX);
   if (stat(cpath, &stat_buf) != 0)
     mrb_sys_fail(mrb, cpath);
-  strcpy(RSTRING_PTR(result), ctime(&stat_buf.st_mtime))
+  strcpy(RSTRING_PTR(result), ctime(&stat_buf.st_mtime));
   mrb_str_resize(mrb, result, strlen(RSTRING_PTR(result)));
   return result;
 }
